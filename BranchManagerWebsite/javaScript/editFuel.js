d96110d2228d9e675a10955df07a897e964592c9
Futoon2002 <fetoonalsulami@gmail.com>
2023-11-18 21:11:26 +0300

    change alert style

--- BranchManagerWebsite/javaScript/editFuel.js
+++ BranchManagerWebsite/javaScript/editFuel.js
@@ -169,7 +169,7 @@ const form = document.getElementById("editStation");
             if (checkedCheckboxes.length > 0) {
                 const checkedRadioButtons = Array.from(radioButtons).filter(radio => radio.checked);
                 if (checkedRadioButtons.length === 0) {
-                    alert(`Please select the status for Fuel Type ${fuelType}`);
+                    showAlert(`Please select the status for Fuel Type ${fuelType}`);
                     return; // Prevent form submission if a validation check fails
                 }
 
@@ -183,7 +183,7 @@ const form = document.getElementById("editStation");
 
             window.location.href = "homepagePM.html";
         } else {
-            alert("Please select at least one checkbox for each fuel type.");
+            showAlert("Please select at least one checkbox for each fuel type");
         }
 
     });
@@ -228,3 +228,67 @@ async function AddStation() {
         console.error("Error updating data in Firestore: ", error);
     }
 }
+
+// change alert style
+function showAlert(message) {
+    var overlay = document.createElement('div');
+    overlay.style.position = 'fixed';
+    overlay.style.top = '0';
+    overlay.style.left = '0';
+    overlay.style.width = '100%';
+    overlay.style.height = '100%';
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    overlay.style.display = 'flex';
+    overlay.style.alignItems = 'center';
+    overlay.style.justifyContent = 'center';
+    overlay.style.zIndex = '9999';
+  
+    var customAlert = document.createElement('div');
+    customAlert.style.backgroundColor = '#fff';
+    customAlert.style.padding = '20px';
+    customAlert.style.border = '1px solid #ccc';
+    customAlert.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+    customAlert.style.textAlign = 'center';
+    customAlert.style.display = 'flex';
+    customAlert.style.flexDirection = 'column'; // Align items in a column
+  
+    var header = document.createElement('div');
+    header.style.display = 'flex';
+    header.style.alignItems = 'center';
+    header.style.marginBottom = '10px'; // Spacing between header and message
+  
+    var imgElement = document.createElement('img');
+    imgElement.src = '../images/logo_no_bkg.png'; // Add your image path here
+    imgElement.style.width = '50px';
+    imgElement.style.marginRight = '10px'; // Space between image and text
+  
+    var headerText = document.createElement('span');
+    headerText.textContent = '91 Website'; // Your header text
+    headerText.style.color = '#000';
+  
+    var messageElement = document.createElement('span');
+    messageElement.textContent = message;
+    messageElement.style.color = '#000';
+  
+    var closeButton = document.createElement('button');
+    closeButton.textContent = 'OK';
+    closeButton.style.padding = '3px 8px'; // Adjust button size
+    closeButton.style.cursor = 'pointer';
+    closeButton.style.border = 'none';
+    closeButton.style.backgroundColor = 'rgba(248, 167, 26)';
+    closeButton.style.color = '#fff';
+    closeButton.style.marginTop = '10px';
+    closeButton.style.alignSelf = 'flex-end'; // Align button to the right
+  
+    closeButton.addEventListener('click', function () {
+      document.body.removeChild(overlay);
+    });
+  
+    header.appendChild(imgElement);
+    header.appendChild(headerText);
+    customAlert.appendChild(header);
+    customAlert.appendChild(messageElement);
+    customAlert.appendChild(closeButton);
+    overlay.appendChild(customAlert);
+    document.body.appendChild(overlay);
+  }
